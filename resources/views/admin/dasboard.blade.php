78310d0bcd66d7358b2f7efee44b23cdfee90b5f
Hikirion <anatoliy.matvienkov38@gmail.com>
2019-07-30 12:58:25 +0300

    Initial commit

---
+++ resources/views/admin/dasboard.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('https://use.fontawesome.com/releases/v5.8.2/css/all.css')}}">
+    <!-- Bootstrap core CSS -->
+    <link href="{{asset('admin/css/bootstrap.min.css')}}" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="{{asset('admin/css/mdb.min.css')}}" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="{{asset('admin/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body class="grey lighten-3">
+<header>
+    <nav class="navbar-fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand waves-effect">
+                <strong class="blue-text"></strong>
+            </a>
+
+        </div>
+
+    </nav>
+
+</header>
+
+<!-- Start your project here-->
+
+<!-- SCRIPTS -->
+<!-- JQuery -->
+<script type="text/javascript" src="{{asset('admin/js/jquery-3.4.1.min.js')}}"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="{{asset('admin/js/popper.min.js')}}"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="{{asset('admin/js/bootstrap.min.js')}}"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="{{asset('admin/js/mdb.min.js')}}"></script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/pricing', function () {
+    return view('infopage.pricing');
+});
+Route::get('/about', function () {
+    return view('infopage.about');
+});
+Route::get('/product', function () {
+    return view('infopage.product');
+});
+Route::get('/contact', function () {
+    return view('infopage.contact');
+});
+
+//==========Админка================
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
